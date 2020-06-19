35ac8dcef849e43e8cff45f1a07e44c8f5ec7a4c
cneyrand <come.neyrand@ens-rennes.fr>
2020-06-19 12:01:22 +0200

    ajout decrease_card

--- wf_finset.v
+++ wf_finset.v
@@ -5,7 +5,7 @@ Section wfrel.
 
 Variable T : finType.
 
-Variable rel : T -> T -> Prop.
+Variable rel : T -> T -> bool.
 
 Hypothesis rel_trans :
   forall (t1 t2 t3 : T), rel t1 t2 -> rel t2 t3 -> rel t1 t3.
@@ -16,9 +16,15 @@ Hypothesis rel_anti_refl :
 Hypothesis rel_anti_sym :
   forall (t1 t2 : T), rel t1 t2 -> rel t2 t1 -> t1 = t2.
 
-Record subSetRel (t : T) := 
-  {set : {set T}; _ : forall t2 : T, rel t2 t <-> t2 \in set}. 
+Definition subSetRel (t : T) := [set t2| rel t2 t].
 
+Lemma decrease_card :
+  forall (t1 t2 : T),
+  t2 \in subSetRel t1 -> #|subSetRel t2| < #|subSetRel t1|.
+Proof.
+have proper t1 t2 : t2 \in subSetRel t1 -> subSetRel t2 \proper subSetRel t1.
+
+Abort.
 Lemma wf_rel : forall t : T, t \in set -> Acc rel t.
 Proof.
 Admitted.
