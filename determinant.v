50679b1e238b64049c406218f6f6388d205ad67c
Léon <leonducruet@yahoo.com>
2022-06-28 16:20:57 +0200

    Update determinant.v

--- determinant.v
+++ determinant.v
@@ -3,7 +3,7 @@ From mathcomp Require Import all_ssreflect all_algebra.
 Require Import matrix.
 
 From mathcomp Require Import ring.
-Import GRing.Theory Num.Theory.
+Import GRing.Theory Num.Theory Order.POrderTheory.
  
 Open Scope ring_scope.
 
@@ -17,7 +17,7 @@ repeat rewrite (expand_det_col _ ord0) /cofactor /row' /col' !big_ord_recr
    det_mx00, mul1r, mulNr, mulrN, opprK, mulr1, addrA) /=.
 
 Definition pt_norm (A : R * R) := 
-  A.1 * A.1 + A.2 * A.2.
+  A.1 ^+2 + A.2 ^+2.
 
 Definition tr_area (A B C : R * R) :=
   \det (area_mx R pt_norm A B C).
@@ -50,11 +50,43 @@ Qed.
 Definition translation (A O : R * R) :=
   (A.1 - O.1, A.2 - O.2).
 
+Lemma translation0 (A : R * R) : translation A A = (0, 0).
+Proof.
+by rewrite/translation !subrr.
+Qed.
+
+Lemma norm_translation :
+  forall A B : R * R,
+  A != B -> pt_norm (translation A B) > 0.
+Proof.
+move=> [Ax Ay] [Bx By].
+rewrite xpair_eqE -(subr_eq0 Ax) -(subr_eq0 Ay)/pt_norm/translation=>/=/nandP[]h.
+  apply: ltr_spaddl; last by apply: sqr_ge0.
+  rewrite lt_def.
+  apply/andP.
+  split; last by apply: sqr_ge0.
+  apply: (contra _ h).
+  by rewrite  sqrf_eq0.
+apply: ltr_spaddr; last by apply: sqr_ge0.
+rewrite lt_def.
+apply/andP.
+split; last by apply: sqr_ge0.
+apply: (contra _ h).
+by rewrite sqrf_eq0.
+Qed.
+
 Lemma area_translation (A B C O : R * R) :
   tr_area A B C =
   tr_area (translation A O) (translation B O) (translation C O).
 Proof.
 by rewrite !poly_area/translation/=; field.
+Qed.
+
+Lemma area_add (A B C D : R * R) :
+  (tr_area A B C) + (tr_area A D B) =
+    (tr_area A D C) + (tr_area B C D).
+Proof.
+by rewrite !poly_area; field.
 Qed.
 
 Definition out_circle (A B C D : R * R) :=
@@ -86,6 +118,23 @@ move => [a_x a_y][b_x b_y][c_x c_y][d_x d_y].
 by rewrite !poly_out_circle; field.
 Qed.
 
+Lemma out_circle_translation (A B C D O : R * R) :
+  out_circle A B C D =
+  out_circle (translation A O) (translation B O) (translation C O) (translation D O).
+Proof.
+by rewrite !poly_out_circle/translation/=; field.
+Qed.
+
+Lemma develop_out_circle (A B C D : R * R) :
+  out_circle A B C D =
+    - (pt_norm A) * (tr_area B C D) +
+    (pt_norm B) * (tr_area A C D) -
+    (pt_norm C) * (tr_area A B D) +
+    (pt_norm D) * (tr_area A B C).
+Proof.
+by rewrite poly_out_circle !poly_area/pt_norm; field.
+Qed.
+
 Definition power (A B C E: R * R) :=
   out_circle A B C E / tr_area A B C.
 
@@ -108,12 +157,14 @@ move=> ABC ADB; field.
 by apply/andP.
 Qed.
 
-Lemma power_translation (A B C D O : R * R) :
-  tr_area A B C != 0 ->
-  power A B C D =
-    power (translation A O) (translation B O) (translation C O) (translation D O).
+Lemma poly_tetrahedron (A B C D : R * R * R) :
+  (\det (tetrahedron_mx R A B C D)) =
+    A.1.1 * (B.1.2 * (C.2 - D.2) - C.1.2 * (B.2 - D.2) + D.1.2 * (B.2 - C.2)) -
+    B.1.1 * (A.1.2 * (C.2 - D.2) - C.1.2 * (A.2 - D.2) + D.1.2 * (A.2 - C.2)) +
+    C.1.1 * (A.1.2 * (B.2 - D.2) - B.1.2 * (A.2 - D.2) + D.1.2 * (A.2 - B.2)) -
+    D.1.1 * (A.1.2 * (B.2 - C.2) - B.1.2 * (A.2 - C.2) + C.1.2 * (A.2 - B.2)).
 Proof.
-by rewrite /power -area_translation !poly_out_circle/translation=>ABC/=; field.
+by expand_det; field.
 Qed.
 
 Definition tr_measure (A B C : R * R) :=
@@ -149,7 +200,7 @@ B.1 *
   C.2 * (A.1 * A.1 + A.2 * A.2 - (C.1 * C.1 + C.2 * C.2)) +
   C.2 * (A.1 * A.1 + A.2 * A.2))).
 Proof.
-by rewrite/tr_measure/pt_norm; expand_det; field.
+by rewrite/tr_measure/pt_norm !poly_tetrahedron/=; field.
 Qed.
 
 Lemma measure_sub_out_circle (A B C D : R * R) :
@@ -207,6 +258,39 @@ by rewrite invr_gt0.
 Qed.
 
 End decrease.
+
+Section flip.
+
+Variables A B C D : R * R.
+
+Hypothesis ABC_gt0 : tr_area A B C > 0.
+
+Hypothesis ACD_gt0 : tr_area A C D > 0.
+
+Hypothesis in_circle : out_circle A B C D <= 0.
+
+Lemma ccw_flip : tr_area A B D > 0.
+Proof.
+rewrite (area_translation _ _ _ A)
+  -(@pmulr_lgt0 _ (pt_norm (translation C A)) _); last first.
+  apply: norm_translation.
+  apply/negP=>/eqP CA.
+  move: ACD_gt0.
+  by rewrite CA dupl_tr_area lt_irreflexive.
+move: in_circle.
+rewrite (out_circle_translation _ _ _ _ A) develop_out_circle/translation
+          2!{1}subrr -!/(translation _ _) /pt_norm(expr2 0) mulr0 addr0 oppr0
+          mul0r add0r -!/(pt_norm _) addrC addrA subr_le0 (mulrC (_ (_ C A))).
+apply: lt_le_trans.
+rewrite -!area_translation.
+apply: addr_gt0; apply: mulr_gt0=>//; apply: norm_translation; apply/eqP=> h.
+  move: ACD_gt0.
+  by rewrite h flipr_tr_area dupl_tr_area oppr0 lt_irreflexive.
+move: ABC_gt0.
+by rewrite h dupl_tr_area lt_irreflexive.
+Qed.
+
+End flip.
 
 End det.
 
